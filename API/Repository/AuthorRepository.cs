e1166a06b6dec59444a82fbe5decd8ed0cb17e7d
Joao Martins <milanezijoao51@gmail.com>
2024-02-15 12:12:22 -0300

    Creating pattern repository for Author

---
+++ API/Repository/AuthorRepository.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Models;
+using API.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Repository
+{
+    public class AuthorRepository : IAuthorRepository
+    {
+        private readonly AppDbContext _context;
+
+        public AuthorRepository(AppDbContext context)
+        {
+            _context=context;
+        }
+        public async Task<List<Author>> GetAllAsync()
+        {
+            return await _context.Authors.ToListAsync();
+        }
+
+        public async Task<Author?> GetByIdAsync(int id)
+        {
+           var author=await _context.Authors.FirstOrDefaultAsync(x=>x.Id==id);
+           if (author is null)return null;
+           return author;
+        }
+        public async Task<Author?> CreateAsync(Author author)
+        {
+            if(author is null) return null;
+            await _context.Authors.AddAsync(author);
+            await _context.SaveChangesAsync();
+            return author;
+        }
+
+        public async Task<Author?> DeleteAsync(int id)
+        {
+            var author=await _context.Authors.FirstOrDefaultAsync(x=>x.Id==id);
+            if (author is null)return null;
+            _context.Authors.Remove(author);
+            await _context.SaveChangesAsync();
+            return author;
+        }
+        public async Task<Author?> UpdateAsync(int id,Author author)
+        {
+            var authorModel=await _context.Authors.FirstOrDefaultAsync(x=>x.Id==id);
+            if (authorModel is null)return null;
+            authorModel.Name=author.Name;
+            await _context.SaveChangesAsync();
+            return authorModel;
+        }
+    }
+}
---
+++ API/Repository/Interfaces/IAuthorRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models;
+
+namespace API.Repository.Interfaces
+{
+    public interface IAuthorRepository
+    {
+        Task<List<Author>> GetAllAsync();
+        Task<Author?> GetByIdAsync(int id);
+        Task<Author?> CreateAsync(Author author);
+        Task<Author?> UpdateAsync(int id,Author author);
+        Task<Author?> DeleteAsync(int id);
+     }
+}
